f0b3faf3adc690e2228d45d0d0e72dc9270261a0
Janderson Souza <jandersonsouza.ti@gmail.com>
2019-02-16 20:52:14 -0300

    Criando componente form-page

---
+++ src/app/core/components/form-page/form-page.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormPageComponent } from './form-page.component';
+
+@NgModule({
+  declarations: [FormPageComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class FormPageModule { }
