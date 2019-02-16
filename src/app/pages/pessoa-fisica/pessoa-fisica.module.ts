c7509450a66a919760cfcf4f9774ee18726bf09b
Janderson Souza <jandersonsouza.ti@gmail.com>
2019-02-16 19:15:39 -0300

    Adicionando campos de consulta a tela de consulta de pessoas.

--- src/app/pages/pessoa-fisica/pessoa-fisica.module.ts
+++ src/app/pages/pessoa-fisica/pessoa-fisica.module.ts
@@ -5,13 +5,22 @@ import { PessoaFisicaRoutingModule } from './pessoa-fisica-routing.module';
 import { FormPessoaFisicaComponent } from './form-pessoa-fisica/form-pessoa-fisica.component';
 import { FindPessoaFisicaComponent } from './find-pessoa-fisica/find-pessoa-fisica.component';
 import { FindPageModule } from 'src/app/core/components/find-page/find-page.module';
+import { MatGridListModule, MatFormFieldModule, MatInputModule, MatSelectModule } from '@angular/material';
+import {NgxMaskModule} from 'ngx-mask';
 
 @NgModule({
   declarations: [FormPessoaFisicaComponent,  FindPessoaFisicaComponent],
   imports: [
     CommonModule,
     PessoaFisicaRoutingModule,
-    FindPageModule
+    FindPageModule,
+    MatGridListModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+
+    NgxMaskModule.forRoot()
+
   ]
 })
 export class PessoaFisicaModule { }
