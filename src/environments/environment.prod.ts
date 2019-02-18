b258dd5c9ed4b56cb833c76bd902b8ebab072b5f
Janderson Souza <jandersonsouza.ti@gmail.com>
2019-02-17 23:29:51 -0300

    Adicionando FirebaseHost

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyB3w9deeJYJwdu9ms2jk25DtyRAEAsIt5Y",
+    authDomain: "desafio-fortbrasil.firebaseapp.com",
+    databaseURL: "https://desafio-fortbrasil.firebaseio.com",
+    projectId: "desafio-fortbrasil",
+    storageBucket: "desafio-fortbrasil.appspot.com",
+    messagingSenderId: "38931349721"
+  }
 };
